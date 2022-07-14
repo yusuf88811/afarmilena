c81fb01eeabbeb3d78b1e2b90d66e29f01b185fa
yusuf88811 <tolaganovyusuf00@gmail.com>
2022-07-14 15:45:09 +0500

    commit_12

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', BlacklistRefreshView.as_view()),
+
     
 ]
