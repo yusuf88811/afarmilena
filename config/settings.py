e01ac79c8ffb3c5a98447b4fd5c77ef69a4417a0
yusuf88811 <tolaganovyusuf00@gmail.com>
2022-07-16 20:57:30 +0500

    commit_66

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,6 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ["172.105.76.9"]
 ALLOWED_HOSTS = ["*"]
-CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -48,7 +47,6 @@ INSTALLED_APPS = [
     'rest_framework',
     "rest_framework_simplejwt",
     "drf_yasg",
-    # "corsheaders",
     # may_apps
     'accounts',
     "service",
@@ -57,10 +55,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-
-    # "corsheaders.middleware.CorsMiddleware",
-    # "django.middleware.common.CommonMiddleware",
-
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
