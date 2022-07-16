479c2da87ed83f3828eef4c8fefd69585e5109af
yusuf88811 <tolaganovyusuf00@gmail.com>
2022-07-16 19:36:57 +0500

    commit_60

--- config/settings.py
+++ config/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'rest_framework',
     "rest_framework_simplejwt",
     "drf_yasg",
-    # "corsheaders",
+    "corsheaders",
     # may_apps
     'accounts',
     "service",
