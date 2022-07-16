a5c798718ed7757da03070e24024fd8edd23fecd
yusuf88811 <tolaganovyusuf00@gmail.com>
2022-07-16 17:37:53 +0500

    commit_55

--- config/settings.py
+++ config/settings.py
@@ -198,13 +198,13 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 #   'http://localhost:3000',
 # )
 
-ALLOWED_HOSTS = ['http://localhost:3000']
-
-
-CORS_ORIGIN_ALLOW_ALL = False
-
-CORS_ORIGIN_WHITELIST = (
-
-    'http://localhost:3000',
-
-)
+# ALLOWED_HOSTS = ['http://localhost:3000']
+#
+#
+# CORS_ORIGIN_ALLOW_ALL = False
+#
+# CORS_ORIGIN_WHITELIST = (
+#
+#     'http://localhost:3000',
+#
+# )
